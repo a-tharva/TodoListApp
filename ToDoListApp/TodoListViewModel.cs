a3eefef6ce46949b9bde1395b8c73e68858538c2
atharva bhandvalkar <bhandvalkaratharva@gmail.com>
2022-03-31 12:55:07 +0530

    added checkbox

--- ToDoListApp/TodoListViewModel.cs
+++ ToDoListApp/TodoListViewModel.cs
@@ -9,17 +9,20 @@ namespace ToDoListApp
 {
     internal class TodoListViewModel
     {
+        // Creating todo list
         public ObservableCollection<TodoItem> TodoItems { get; set; }
         public TodoListViewModel()
         {
             TodoItems = new ObservableCollection<TodoItem>();
 
-            TodoItems.Add(new TodoItem("Todo 1", false));
+            TodoItems.Add(new TodoItem("Todo 1", true));
             TodoItems.Add(new TodoItem("Todo 2", false)); 
             TodoItems.Add(new TodoItem("Todo 3", false));
 
             
         }
+
+        // Add to list
         public ICommand AddTodoCommand => new Command(AddTodoItem);
         public string NewTodoInputValue { get; set; }
         void AddTodoItem()
@@ -27,6 +30,7 @@ namespace ToDoListApp
             TodoItems.Add(new TodoItem(NewTodoInputValue, false));
         }
 
+        // Remove from list
         public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
         void RemoveTodoItem(object o)
         {
